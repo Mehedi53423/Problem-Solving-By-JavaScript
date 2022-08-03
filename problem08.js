6ffd40736eff9fd78e8f126d4e5c2c7e3673b5ae
Md. Mehedi Hasan <mehedi53423@gmail.com>
2022-08-04 00:31:52 +0600

    Problem 8 (Solved)

---
+++ problem08.js
@@ -0,0 +1,17 @@
+var side1 = 9;
+var side2 = 8;
+var side3 = 9;
+
+if (side1 == side2) {
+  if (side1 == side3) {
+    console.log("Triangle Is Not Isosceles");
+  } else {
+    console.log("Triangle Is Isosceles");
+  }
+} else {
+  if (side1 == side3) {
+    console.log("Triangle Is Isosceles");
+  } else {
+    console.log("Triangle Is Not Isosceles");
+  }
+}
