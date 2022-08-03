d900be0452c7530664f8425a76b2569552ff1a76
Md. Mehedi Hasan <mehedi53423@gmail.com>
2022-08-03 07:45:16 +0600

    Problem 1 (Solved)

--- problem01.js
+++ problem01.js
@@ -1,3 +1,8 @@
 const prompt = require("prompt-sync")({ sigint: true });
 
-var name = prompt("Enter Your Name");
+var money = prompt("Given Money : ");
+var price = prompt("The Cost Of 1 KG Of Oranges & 1 KG Of Apples : ");
+
+console.log(
+  "The Shopkeeper Will Return : " + (parseInt(money) - parseInt(price))
+);
